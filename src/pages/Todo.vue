ecdade80629d131966d53da7d7b7dc31ed32ec72
Maxwell Alexius <destinydragon85614@gmail.com>
2019-01-10 16:31:46 +0800

    Create the main app page

---
+++ src/pages/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>TODO Blank Slate</h1>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+
+  // data() {},
+  // computed: {},
+  // watch: {},
+  // updated() {},
+  // mounted() {},
+  // beforeDestroy() {},
+  // destroyed() {},
+};
+</script>
+
+<style>
+</style>
